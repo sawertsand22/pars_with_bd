3e3aae6f83cb4beccfd33b6cb7165ecd893fe736
sawertsand22 <85335401+sawertsand22@users.noreply.github.com>
2023-07-26 17:04:21 +0300

    Cmake build

---
+++ build-pars-unknown-Debug/CMakeFiles/pars.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pars_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/pars_autogen.dir/ParseCache.txt"
+  "pars_autogen"
+  "CMakeFiles/pars.dir/main.cpp.o"
+  "CMakeFiles/pars.dir/main.cpp.o.d"
+  "CMakeFiles/pars.dir/pars_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/pars.dir/pars_autogen/mocs_compilation.cpp.o.d"
+  "pars"
+  "pars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-pars-unknown-Debug/CMakeFiles/pars.dir/pars_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/pars.dir/pars_autogen/mocs_compilation.cpp.o: \
+ /home/sawert/qtprojects/build-pars-unknown-Debug/pars_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
